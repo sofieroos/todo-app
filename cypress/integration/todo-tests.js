2d40d796f99da9e70a6ae8ac09ad700355543fd6
sofieroos <sofie.roos@omegapoint.se>
2020-06-12 12:47:01 +0200

    Empty testfile

---
+++ cypress/integration/todo-tests.js
@@ -0,0 +1,54 @@
+/// <reference types="cypress" />
+import dayjs from "dayjs";
+
+context("Actions", () => {
+  const dateString = "2020-03-13 12:00";
+  const date = dayjs(dateString);
+
+  beforeEach(() => {
+    // Visit the webpage
+  });
+
+  it("adds a new todo and the todo is appended to the list", () => {
+    // Find the input field for new todo and type a description into it
+    // Click the button to add the todo
+    // Assert that the todo was added to the list
+  });
+
+  it("does not add a new todo if only a space is typed into the input field", () => {
+    // Type an empty description into the input field
+    // Click the button to add the todo
+    // Assert that todo was not added
+  });
+
+  it("deletes a todo and the todo is deleted from the list", () => {
+    // Get the first todo item and locate the button within it then click it
+    // Assert that the item was removed
+  });
+
+  it("completes a todo and the todo is checked", () => {
+    // Get the the last todo item, find the checkbox and click it
+    // Assert that the checkbox is checked
+  });
+
+  it("adds a new todo with a deadline and the todo is appended to the list", () => {
+    // Add new todo
+    // Get the deadline input and save it as a variable
+    // Get the saved input and click it
+    // Fidn the OK button in the date-dialog
+    // Assert that the title of the dateinput has correct value
+    // Trigger Mouseover on the date input to make the tooltip visible
+    // Assert that the tooltip of the dateinput has correct value
+    // Trigger Mouseleave on the date input to make the tooltip dissapear
+    // Add the todo
+    // Assert that the todo was added
+    // Assert that the date is correct on the new todo-item
+  });
+
+  it("removes a todo after it's been completed for day", () => {
+    // Setup the clock to overide the date
+    // Get the the last todo item, find the checkbox and click it
+    // Move the clock forward 24h + 100ms (86400100)
+    // Assert that the item was removed
+  });
+});
